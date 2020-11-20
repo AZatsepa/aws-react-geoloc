72e434cddb28d3ae42f1675847ae5f5925573271
Artem Zatsepa <artem.zatsepa@nixsolutions.com>
2020-11-20 13:19:32 +0200

    Add routing

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import { Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { withRouter } from 'react-router-dom';
+
+const NavBar = (props) => {
+  const handleLogout = (event) => {
+    event.preventDefault();
+    // auth logic here
+    console.log('Logged out');
+    props.history.push("/");
+  };
+
+  return(
+    <Navbar bg="light" expand="lg">
+      <LinkContainer to="/">
+        <Navbar.Brand>AWS-React-Geoloc</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+          <Nav.Link href="/" onClick={handleLogout}>Logout</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default withRouter(NavBar);
